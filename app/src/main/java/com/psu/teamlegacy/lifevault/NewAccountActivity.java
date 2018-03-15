7453c260c136f1d7c5e8c29e8bba7b87d9e33e5e
khaoticx <lnb5606@gmail.com>
2018-03-15 10:45:31 -0400

    Implementing secure login.....

--- app/src/main/java/com/psu/teamlegacy/lifevault/NewAccountActivity.java
+++ app/src/main/java/com/psu/teamlegacy/lifevault/NewAccountActivity.java
@@ -1,6 +1,9 @@
 package com.psu.teamlegacy.lifevault;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +12,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,30 +31,53 @@ public class NewAccountActivity extends AppCompatActivity {
         newAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                if (validateLogin()){
-                    if(validatePassword()){
-                        if(((EditText) findViewById(R.id.passwordField)).getText().toString().equals(
-                                ((EditText) findViewById(R.id.confirmPasswordField)).getText().toString())){
-                            generateNewAccount();
-                        }
-                        else{
+            if (validateLogin()){
+                if (loginDoesNotExists()){
+                    if (validatePassword()) {
+                        if (((EditText) findViewById(R.id.passwordField)).getText().toString().equals(
+                                ((EditText) findViewById(R.id.confirmPasswordField)).getText().toString())) {
+                            try {
+                                generateNewAccount();
+                            } catch (NoSuchAlgorithmException e) {
+                                //e.printStackTrace();
+                            }
+                        } else {
                             Toast.makeText(getApplicationContext(),
                                     "Passwords do not match", Toast.LENGTH_LONG).show();
                         }
-                    }
-                    else {
-                        Log.d("PASSWORD_INVALID", "PASSWORD NOT VALID: " +
-                                ((EditText) findViewById(R.id.passwordField)).getText().toString());
-                        Toast.makeText(getApplicationContext(),
-                                "Please enter a valid password", Toast.LENGTH_LONG).show();
+                    } else {
+                        AlertDialog alertDialog = new AlertDialog.Builder(NewAccountActivity.this).create();
+                        alertDialog.setTitle("Invalid Password");
+                        alertDialog.setMessage("\n1) Password must be 8 - 20 characters long" +
+                                "\n2) Must have at least one capital and lower case letter" +
+                                "\n3) Must have at least one numeral digit" +
+                                "\n4) Must have at least one special character");
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
                     }
                 }
-                else{
-                    Log.d("ID_NOT_VALID", "ID NOT VALID:" +
-                            ((EditText) findViewById(R.id.nameTextField)).getText().toString());
+                else {
                     Toast.makeText(getApplicationContext(),
-                            "Please enter a valid login ID", Toast.LENGTH_LONG).show();
+                            "Login ID not available", Toast.LENGTH_LONG).show();
                 }
+            }
+            else {
+                AlertDialog alertDialog = new AlertDialog.Builder(NewAccountActivity.this).create();
+                alertDialog.setTitle("Invalid ID");
+                alertDialog.setMessage("\nLogin IDs must start with a letter and consist only of letters, numbers, and underscores");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
             }
         });
 
@@ -64,7 +94,7 @@ public class NewAccountActivity extends AppCompatActivity {
     }
 
     private boolean validateLogin(){
-        String loginID = ((EditText) findViewById(R.id.nameTextField)).getText().toString();
+        String loginID = ((EditText) findViewById(R.id.loginIdField)).getText().toString();
         Pattern pattern;
         Matcher matcher;
         final String LOGIN_PATTERN = "^[A-z]([A-z0-9_]){1,20}$";
@@ -73,6 +103,19 @@ public class NewAccountActivity extends AppCompatActivity {
         matcher = pattern.matcher(loginID);
 
         return matcher.matches();
+    }
+
+    private boolean loginDoesNotExists(){
+        File file = new File(getApplicationContext().getFilesDir(),((EditText) findViewById(R.id.loginIdField)).getText().toString()+ ".txt");
+
+        if(file.exists()){
+            Log.d("FILE_EXISTS", "login exists");
+            return false;
+        }
+        else{
+            Log.d("FILE_EXISTS", "login does not exists");
+            return true;
+        }
     }
 
     private boolean validatePassword(){
@@ -87,13 +130,36 @@ public class NewAccountActivity extends AppCompatActivity {
         return matcher.matches();
     }
 
-    private void generateNewAccount(){
+    private void generateNewAccount() throws NoSuchAlgorithmException {
+        byte[] passwordByte = ((EditText) findViewById(R.id.passwordField)).getText().toString().getBytes();
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(passwordByte);
+        byte[] newHash = md.digest();
 
+
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(((EditText) findViewById(R.id.loginIdField)).getText().toString()+ ".txt", Context.MODE_PRIVATE);
+            outputStream.write(newHash);
+            outputStream.close();
+        } catch (Exception e) {
+            Log.e("FILE ERROR", "Error during file processing.");
+        }
+
+        Toast.makeText(getApplicationContext(),
+                "New account created", Toast.LENGTH_LONG).show();
+        String[] str = this.fileList();
+        for (String s: str) {
+            //Do your stuff here
+            Log.wtf("ERRORRRRRR", s);
+        }
     }
 
     private void goToMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
 }
 
